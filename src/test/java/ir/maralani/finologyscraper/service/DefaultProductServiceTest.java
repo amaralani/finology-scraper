23ed6648098a097968a6fe53419dea14d3af3a7b
Amir Maralani <amir.maralani@gmail.com>
2020-10-28 18:25:09 +0330

    A few more tests

--- src/test/java/ir/maralani/finologyscraper/service/DefaultProductServiceTest.java
+++ src/test/java/ir/maralani/finologyscraper/service/DefaultProductServiceTest.java
@@ -1,20 +1,72 @@
 package ir.maralani.finologyscraper.service;
 
+import ir.maralani.finologyscraper.model.Product;
+import ir.maralani.finologyscraper.repository.ProductRepository;
 import ir.maralani.finologyscraper.service.product.DefaultProductService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tests for {@link DefaultProductService}.
+ */
 @SpringBootTest
 public class DefaultProductServiceTest {
+
+    /**
+     * Actual service to be tested.
+     */
     @Autowired
     public DefaultProductService defaultProductService;
 
+    /**
+     * Mocked Product DAO.
+     */
+    @MockBean
+    public ProductRepository productRepositoryMock;
+
+    /**
+     * Dummy list for products.
+     */
+    private List<Product> dummyProductList;
+
+    @BeforeEach
+    public void before() {
+        dummyProductList = new ArrayList<>();
+        dummyProductList.add(createProduct("1"));
+        dummyProductList.add(createProduct("b"));
+        Mockito.doReturn(dummyProductList).when(productRepositoryMock).findAll();
+    }
+
     @Test
-    public void getScannedPages(){
+    public void getScannedPages() {
         Assertions.assertThat(defaultProductService.getScannedPages()).isEmpty();
         defaultProductService.getScannedPages().add("new path");
         Assertions.assertThat(defaultProductService.getScannedPages().size()).isEqualTo(1);
+    }
+
+    @Test
+    public void getProductList() {
+        List<Product> allProducts = defaultProductService.getAllProducts();
+
+        Assertions.assertThat(allProducts).containsAll(dummyProductList);
+    }
+
+
+    private Product createProduct(String id) {
+        Product product = new Product();
+        product.setName("name" + id);
+        product.setDescription("description" + id);
+        product.setPath("path" + id);
+        product.setPrice("price" + id);
+        product.setExtraInformation("extra-info" + id);
+        return product;
     }
 }
--- src/test/java/ir/maralani/finologyscraper/service/DefaultScraperServiceTest.java
+++ src/test/java/ir/maralani/finologyscraper/service/DefaultScraperServiceTest.java
@@ -14,19 +14,19 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 import java.io.IOException;
 import java.net.SocketTimeoutException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
 
 /**
  * Tests related to {@link DefaultScraperService}.
  *
  * @author Amir
  */
-@SpringBootTest(classes = TestRedisConfiguration.class, properties = "spring.main.allow-bean-definition-overriding=true")
+@SpringBootTest(classes = TestRedisConfiguration.class,
+        properties = "spring.main.allow-bean-definition-overriding=true")
 public class DefaultScraperServiceTest {
     /**
      * Injected service to be tested.
@@ -61,10 +61,16 @@ public class DefaultScraperServiceTest {
             assertThat(scrapedPage.getProduct()).isNotNull();
             assertThat(scrapedPage.getProduct().getName()).isEqualTo("Breathe-Easy Tank");
             assertThat(scrapedPage.getProduct().getDescription())
-                    .isEqualTo("The Breathe Easy Tank is so soft, lightweight, and comfortable, you won't even know it's there -- until its high-tech Cocona® fabric starts wicking sweat away from your body to help you stay dry and focused. Layer it over your favorite sports bra and get moving. • Machine wash/dry. • Cocona® fabric.");
+                    .isEqualTo(
+                            "The Breathe Easy Tank is so soft, lightweight, and comfortable, you won't even know it's" +
+                                    " there -- until its high-tech Cocona® fabric starts wicking sweat away from your" +
+                                    " body to help you stay dry and focused. Layer it over your favorite sports bra " +
+                                    "and get moving. • Machine wash/dry. • Cocona® fabric.");
             assertThat(scrapedPage.getProduct().getPrice()).isEqualTo("34");
             assertThat(scrapedPage.getProduct().getExtraInformation())
-                    .isEqualTo("Style: Tank | Material: Cocona® performance fabric, Cotton | Pattern: Solid | Climate: Indoor, Warm ");
+                    .isEqualTo(
+                            "Style: Tank | Material: Cocona® performance fabric, Cotton | Pattern: Solid | Climate: " +
+                                    "Indoor, Warm ");
 
             assertThat(redisTemplate.hasKey(basePath)).isTrue();
         } catch (HttpStatusException | SocketTimeoutException e) {
